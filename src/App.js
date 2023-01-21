a65b5fada5bbc95a3eb57ec639474e4a3cbf7d28
aniiaham <aniya_axm@icloud.com>
2023-01-20 19:27:24 -0500

    second commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import './App.css';
+import "./App.css";
+import Button from "./components/UI/Button/Button";
+import DemoOutput from "./components/UI/Demo/DemoOutput";
 
 function App() {
+  const [showParagraph, setShowParagraph] = useState(false);
+
+  console.log('APP RUNNING')
+  
+  const toggleParagraph = () => {
+    setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+  };
   return (
     <div className="app">
       <h1>Hi there!</h1>
+      <DemoOutput show={showParagraph}/>
+      <Button onClick={toggleParagraph}>Toggle Paragraph!</Button>
     </div>
   );
 }
